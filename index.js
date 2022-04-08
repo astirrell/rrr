14a2b9f72144ff05355162fe8db53bf911ff1d1e
astirrell <92372464+astirrell@users.noreply.github.com>
2022-04-08 09:13:27 +0600

    commit
    next commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// function initMap() {
+  
+//     const uluru = { lat: 54.985554, lng: 73.0759643 };
+    
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//       zoom: 11,
+//       center: uluru,
+//     });
+   
+//     const marker = new google.maps.Marker({
+//       position: uluru,
+//       map: map,
+//     });
+//   }
+
+
+
+
